2d7eff9ba20a6a1258577f1384d4f66aebc6ec62
fedor.grintsov <fedor.grintsov@aristocrat.com>
2024-11-25 16:44:20 +0200

    distortion controller refactoring, clean up

--- ex-404/Assets/Scripts/DistortionController/UIDistortionController.cs
+++ ex-404/Assets/Scripts/DistortionController/UIDistortionController.cs
@@ -7,51 +7,56 @@ using UnityEngine.UI;
 public class UIDistortionController : MonoBehaviour
 {
     [Header("Distortion Offset Animation Curves")]
-    public AnimationCurve topLeftCurveX = AnimationCurve.Linear(0, 0, 1, 0);
-    public AnimationCurve topLeftCurveY = AnimationCurve.Linear(0, 0, 1, 0);
-    public AnimationCurve topRightCurveX = AnimationCurve.Linear(0, 0, 1, 0);
-    public AnimationCurve topRightCurveY = AnimationCurve.Linear(0, 0, 1, 0);
-    public AnimationCurve bottomLeftCurveX = AnimationCurve.Linear(0, 0, 1, 0);
-    public AnimationCurve bottomLeftCurveY = AnimationCurve.Linear(0, 0, 1, 0);
-    public AnimationCurve bottomRightCurveX = AnimationCurve.Linear(0, 0, 1, 0);
-    public AnimationCurve bottomRightCurveY = AnimationCurve.Linear(0, 0, 1, 0);
+    [SerializeField] private AnimationCurve _topLeftCurveX = AnimationCurve.Linear(0, 0, 1, 0);
+    [SerializeField] private AnimationCurve _topLeftCurveY = AnimationCurve.Linear(0, 0, 1, 0);
+    [SerializeField] private AnimationCurve _topRightCurveX = AnimationCurve.Linear(0, 0, 1, 0);
+    [SerializeField] private AnimationCurve _topRightCurveY = AnimationCurve.Linear(0, 0, 1, 0);
+    [SerializeField] private AnimationCurve _bottomLeftCurveX = AnimationCurve.Linear(0, 0, 1, 0);
+    [SerializeField] private AnimationCurve _bottomLeftCurveY = AnimationCurve.Linear(0, 0, 1, 0);
+    [SerializeField] private AnimationCurve _bottomRightCurveX = AnimationCurve.Linear(0, 0, 1, 0);
+    [SerializeField] private AnimationCurve _bottomRightCurveY = AnimationCurve.Linear(0, 0, 1, 0);
 
     [Header("Animation Settings")]
     public float timeMultiplier = 1.0f;
     public bool loopAnimation = true;
-    
+
     [Header("Alpha Cutoff")]
     public bool useAlphaCutoff = true;
 
     [Header("Slicing Borders (Normalized UV Coordinates)")]
     [Tooltip("Set according to your sprite's 9-slice borders.")]
-    public float borderLeft = 0.25f;
-    public float borderBottom = 0.25f;
-    public float borderRight = 0.75f;
-    public float borderTop = 0.75f;
+    public float borderLeft = 0.4f;
+    public float borderBottom = 0.4f;
+    public float borderRight = 0.4f;
+    public float borderTop = 0.4f;
 
-    private Material material;
-    private float timeElapsed;
+    private Material _material;
+    private float _timeElapsed;
 
     private void Start()
     {
-        Image image = GetComponent<Image>();
-        
-    
-        material = new Material(image.material);
-        image.material = material;
+        var image = GetComponent<Image>();
+        if (image.material != null)
+        {
+            _material = new Material(image.material);
+            image.material = _material;
+        }
+        else
+        {
+            Debug.LogError("Image material is not assigned. Please assign a valid material.");
+        }
     }
 
     private void Update()
     {
-        timeElapsed += Time.deltaTime * timeMultiplier;
+        _timeElapsed += Time.deltaTime * timeMultiplier;
 
         if (loopAnimation)
         {
             float maxTime = GetMaxCurveTime();
-            if (timeElapsed > maxTime)
+            if (_timeElapsed > maxTime)
             {
-                timeElapsed = 0;
+                _timeElapsed = 0;
             }
         }
 
@@ -61,34 +66,41 @@ public class UIDistortionController : MonoBehaviour
     private float GetMaxCurveTime()
     {
         float maxTime = Mathf.Max(
-            topLeftCurveX.keys[topLeftCurveX.length - 1].time,
-            topLeftCurveY.keys[topLeftCurveY.length - 1].time,
-            topRightCurveX.keys[topRightCurveX.length - 1].time,
-            topRightCurveY.keys[topRightCurveY.length - 1].time,
-            bottomLeftCurveX.keys[bottomLeftCurveX.length - 1].time,
-            bottomLeftCurveY.keys[bottomLeftCurveY.length - 1].time,
-            bottomRightCurveX.keys[bottomRightCurveX.length - 1].time,
-            bottomRightCurveY.keys[bottomRightCurveY.length - 1].time
+            GetCurveMaxTime(_topLeftCurveX),
+            GetCurveMaxTime(_topLeftCurveY),
+            GetCurveMaxTime(_topRightCurveX),
+            GetCurveMaxTime(_topRightCurveY),
+            GetCurveMaxTime(_bottomLeftCurveX),
+            GetCurveMaxTime(_bottomLeftCurveY),
+            GetCurveMaxTime(_bottomRightCurveX),
+            GetCurveMaxTime(_bottomRightCurveY)
         );
         return maxTime;
     }
 
+    private float GetCurveMaxTime(AnimationCurve curve)
+    {
+        return curve.keys.Length > 0 ? curve.keys[curve.keys.Length - 1].time : 0f;
+    }
+
     private void UpdateShader()
     {
-        Vector2 topLeftOffset = new Vector2(topLeftCurveX.Evaluate(timeElapsed), topLeftCurveY.Evaluate(timeElapsed));
-        Vector2 topRightOffset = new Vector2(topRightCurveX.Evaluate(timeElapsed), topRightCurveY.Evaluate(timeElapsed));
-        Vector2 bottomLeftOffset = new Vector2(bottomLeftCurveX.Evaluate(timeElapsed), bottomLeftCurveY.Evaluate(timeElapsed));
-        Vector2 bottomRightOffset = new Vector2(bottomRightCurveX.Evaluate(timeElapsed), bottomRightCurveY.Evaluate(timeElapsed));
+        if (_material == null) return;
 
-        material.SetVector("_TopLeftOffset", topLeftOffset);
-        material.SetVector("_TopRightOffset", topRightOffset);
-        material.SetVector("_BottomLeftOffset", bottomLeftOffset);
-        material.SetVector("_BottomRightOffset", bottomRightOffset);
+        Vector2 topLeftOffset = new Vector2(_topLeftCurveX.Evaluate(_timeElapsed), _topLeftCurveY.Evaluate(_timeElapsed));
+        Vector2 topRightOffset = new Vector2(_topRightCurveX.Evaluate(_timeElapsed), _topRightCurveY.Evaluate(_timeElapsed));
+        Vector2 bottomLeftOffset = new Vector2(_bottomLeftCurveX.Evaluate(_timeElapsed), _bottomLeftCurveY.Evaluate(_timeElapsed));
+        Vector2 bottomRightOffset = new Vector2(_bottomRightCurveX.Evaluate(_timeElapsed), _bottomRightCurveY.Evaluate(_timeElapsed));
 
-        material.SetFloat("_UseAlphaCutoff", useAlphaCutoff ? 1.0f : 0.0f);
-        material.SetFloat("_BorderLeft", borderLeft);
-        material.SetFloat("_BorderBottom", borderBottom);
-        material.SetFloat("_BorderRight", borderRight);
-        material.SetFloat("_BorderTop", borderTop);
+        _material.SetVector("_TopLeftOffset", topLeftOffset);
+        _material.SetVector("_TopRightOffset", topRightOffset);
+        _material.SetVector("_BottomLeftOffset", bottomLeftOffset);
+        _material.SetVector("_BottomRightOffset", bottomRightOffset);
+
+        _material.SetFloat("_UseAlphaCutoff", useAlphaCutoff ? 1.0f : 0.0f);
+        _material.SetFloat("_BorderLeft", borderLeft);
+        _material.SetFloat("_BorderBottom", borderBottom);
+        _material.SetFloat("_BorderRight", borderRight);
+        _material.SetFloat("_BorderTop", borderTop);
     }
 }
